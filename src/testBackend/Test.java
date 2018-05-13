94c7d74045a21fcf7884901b379848d375d3cbe2
nadaez <31936299+nadaez@users.noreply.github.com>
2018-05-13 13:45:55 +0000

    Update Test.java

--- src/testBackend/Test.java
+++ src/testBackend/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String [] args) {
 		System.out.println("Hello");
 		System.out.println("Another develop add this line");
+			System.out.println("Java is cool");
+
 	}
 	
 	public void method() {
